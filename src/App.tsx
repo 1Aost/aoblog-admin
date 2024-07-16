cc1c4d0f3182f2a7db97705faf7cca206e9431f7
aoyuru <102245971+1Aost@users.noreply.github.com>
2024-07-16 19:54:26 +0800

    refactor:loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React, { Suspense } from 'react'
-import { useRoutes } from "react-router-dom"
-import { Spin } from 'antd';
+import React, { Suspense } from 'react';
+import { useRoutes } from "react-router-dom";
 import routes from './routes';
-import "@/assets/font_style_cn.css"
+import "@/assets/font_style_cn.css";
 import "./App.css";
+import Loading from './components/Loading';
 
 const App = () => {
   const element = useRoutes(routes);
   return (
-    <Suspense fallback={<Spin />}>
+    <Suspense fallback={<Loading />}>
       {element}
     </Suspense>
   )
